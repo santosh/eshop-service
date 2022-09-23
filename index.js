e26ef2bfcda69c29345beb6acd816b126b2501ca
Santosh Kumar <sntshkmr60@gmail.com>
2022-09-23 16:50:44 +0530

    Establish database connection

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const dbConfig = require("./configs/db.config")
+const serverConfig = require("./configs/server.config")
+
+mongoose.connect(dbConfig.DB_URL)
+
+const db = mongoose.connection
+
+db.on("error", () => {
+  console.log("Error while connecting to the MongoDB server");
+})
+
+db.once("open", () => {
+  console.log("Connected to the MongoDB");
+  // init()
+})
 
 const app = express();
-const port = 8000;
 
 app.get('/ping', (req, res) => {
   res.json({ "message": "pong" })
 });
 
 
-app.listen(port);
+app.listen(serverConfig.PORT, () => {
+  console.log("eshop-service is listening on port no:", serverConfig.PORT);
+});
 
 module.exports = app;
